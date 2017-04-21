0c8e0583a9c1e788b045188ba4f881a5bad0eb18
SinisterBlade <princedastan@ymail.com>
2017-04-21 10:57:57 +0530

    Print response text

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
       console.log(err)
     }
     else {
+      console.log(response.output.text)
       rtm.sendMessage(response.output.text, message.channel);
     }
   })
